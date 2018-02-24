53f50351b8752fd3bfe4157477c689ecd4888233
dacho68 <dacho68@gmail.com>
2018-02-24 13:33:47 -0500

    with filder

--- QrWebCam.Demo/MainWindow.xaml.cs
+++ QrWebCam.Demo/MainWindow.xaml.cs
@@ -12,33 +12,71 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace QrWebCam.Demo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            
-        }
+      InitializeComponent();
 
-        private void camSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            webCam.CameraIndex = camSelect.SelectedIndex;
-        }
-
-        private void QrWebCamControl_QrDecoded(object sender, string e)
-        {
-            dtext.Text = "The decoded string is: "+e;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            camSelect.ItemsSource = webCam.CameraNames;
-        }
     }
+
+    private void camSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      webCam.CameraIndex = camSelect.SelectedIndex;
+      webCam.StartDetection();
+    }
+
+    DispatcherTimer mDispatcherTimer;
+
+    private void QrWebCamControl_QrDecoded(object sender, string e)
+    {
+  
+      dtext.Text = "The decoded string is: " + e;
+      MessageBox.Show(e);
+      dtext.Text = "";
+      //webCam.Restart();
+      mDispatcherTimer.Start();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+       mDispatcherTimer = new DispatcherTimer();
+       camSelect.ItemsSource = webCam.CameraNames;
+       mDispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+       mDispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+    }
+    private void DispatcherTimer_Tick(object sender, EventArgs e)
+    {
+      mDispatcherTimer.Stop();
+      webCam.Restart();
+    }
+
+    private void Image_Loaded(object sender, RoutedEventArgs e)
+    {
+      // ... Create a new BitmapImage.
+      BitmapImage b = new BitmapImage();
+      var wCurrentLocation = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      var wImagePath = System.IO.Path.Combine(wCurrentLocation, "image.png");
+      b.BeginInit();
+      b.UriSource = new Uri(wImagePath);
+      b.EndInit();
+
+      // ... Get Image reference from sender.
+      var image = sender as Image;
+      // ... Assign Source.
+      image.Source = b;
+    }
+
+    private void restart_Click(object sender, RoutedEventArgs e)
+    {
+      webCam.Restart();
+    }
+  }
 }
--- QrWebCam/QrWebCamControl.xaml.cs
+++ QrWebCam/QrWebCamControl.xaml.cs
@@ -11,142 +11,160 @@ using ZXing;
 
 namespace QrWebCam
 {
-    /// <summary>
-    ///     Interaction logic for QrWebCamControl.xaml
-    /// </summary>
-    public partial class QrWebCamControl : INotifyPropertyChanged
+  /// <summary>
+  ///     Interaction logic for QrWebCamControl.xaml
+  /// </summary>
+  public partial class QrWebCamControl : INotifyPropertyChanged
+  {
+    #region Public properties
+
+    public MediaPlayer MediaPlayer { get; set; }
+
+    public string BeepSoundPath { get; set; }
+
+    public string StatusText
     {
-        #region Public properties
-
-        public MediaPlayer MediaPlayer { get; set; }
-
-        public string BeepSoundPath { get; set; }
-
-        public string StatusText
-        {
-            get { return _statusText; }
-            set
-            {
-                _statusText = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int CameraIndex
-        {
-            get { return _cameraIndex; }
-            set
-            {
-                _cameraIndex = value;
-                CameraIndexSelectionChanged(value);
-            }
-        }
-
-        public List<string> CameraNames
-        {
-            get { return _cameraNames; }
-            set
-            {
-                _cameraNames = value;
-                OnPropertyChanged();
-            }
-        }
-
-        #endregion
-
-        #region Private vars
-
-        private CameraDevices Cam { get; } = new CameraDevices();
-        private CancellationTokenSource Cts { get; set; }
-
-        private List<string> _cameraNames;
-        private int _cameraIndex;
-        private string _statusText = "No Signal";
-
-        #endregion
-
-        #region Events
-
-        public event EventHandler<string> QrDecoded;
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
-
-        public QrWebCamControl()
-        {
-            InitializeComponent();
-            Dispatcher.ShutdownStarted += Dispatcher_ShutdownStarted;
-            DataContext = this;
-        }
-
-        private void Dispatcher_ShutdownStarted(object sender, EventArgs e)
-        {
-            DisposeCamera();
-        }
-
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            CameraNames = Cam.GetStringCameras();
-        }
-
-        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
-        {
-            DisposeCamera();
-        }
-
-        private void Init(int index)
-        {
-            MediaPlayer = new MediaPlayer {Volume = 100};
-            if (!string.IsNullOrEmpty(BeepSoundPath)) MediaPlayer.Open(new Uri(BeepSoundPath ?? ""));
-            Cam.SelectCamera(index);
-            Cam.Current.NewFrame += CameraNewFrame;
-            Cam.Current.Start();
-            Cts = new CancellationTokenSource();
-        }
-
-        private void DisposeCamera()
-        {
-            Cts?.Cancel();
-            Cam.Current?.SignalToStop();
-            if (Cam.Current != null) Cam.Current.NewFrame -= CameraNewFrame;
-
-            MediaPlayer?.Close();
-            MediaPlayer = null;
-        }
-
-        private void CameraNewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            using (var newFrame = Image.Clone(eventArgs.Frame))
-            {
-                var reader = new BarcodeReader().Decode(newFrame);
-
-                Dispatcher.Invoke(() =>
-                {
-                    if (reader != null)
-                        QrDecoded?.Invoke(this, reader.Text);
-
-                    CamStream.Source = newFrame.Convert();
-                });
-            }
-        }
-
-        private void CameraIndexSelectionChanged(int e)
-        {
-            if (Cam.Devices.Count <= 0) return;
-            if (Cam.Current == null)
-            {
-                Init(e);
-            }
-            else
-            {
-                DisposeCamera();
-                Init(e);
-            }
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+      get { return _statusText; }
+      set
+      {
+        _statusText = value;
+        OnPropertyChanged();
+      }
     }
+
+    public int CameraIndex
+    {
+      get { return _cameraIndex; }
+      set
+      {
+        _cameraIndex = value;
+        CameraIndexSelectionChanged(value);
+      }
+    }
+
+    public List<string> CameraNames
+    {
+      get { return _cameraNames; }
+      set
+      {
+        _cameraNames = value;
+        OnPropertyChanged();
+      }
+    }
+
+
+    
+    public void StartDetection()
+    {
+    
+     
+    }
+
+    public void Restart()
+    {
+      Cam.Current.Start();
+    }
+
+    #endregion
+
+    #region Private vars
+
+    private CameraDevices Cam { get; } = new CameraDevices();
+    private CancellationTokenSource Cts { get; set; }
+
+    private List<string> _cameraNames;
+    private int _cameraIndex;
+    private string _statusText = "No Signal";
+
+    #endregion
+
+    #region Events
+
+    public event EventHandler<string> QrDecoded;
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    public QrWebCamControl()
+    {
+      InitializeComponent();
+      Dispatcher.ShutdownStarted += Dispatcher_ShutdownStarted;
+      DataContext = this;
+    }
+
+    private void Dispatcher_ShutdownStarted(object sender, EventArgs e)
+    {
+      DisposeCamera();
+    }
+
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+      CameraNames = Cam.GetStringCameras();
+    }
+
+    private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+    {
+      DisposeCamera();
+    }
+
+    private int Index { get; set; }
+    private void Init(int index)
+    {
+      Index = index;
+      MediaPlayer = new MediaPlayer { Volume = 100 };
+      if (!string.IsNullOrEmpty(BeepSoundPath)) MediaPlayer.Open(new Uri(BeepSoundPath ?? ""));
+      Cam.SelectCamera(index);
+      Cam.Current.NewFrame += CameraNewFrame;
+      Cam.Current.Start();
+      Cts = new CancellationTokenSource();
+    }
+
+    private void DisposeCamera()
+    {
+      Cts?.Cancel();
+      Cam.Current?.SignalToStop();
+      if (Cam.Current != null) Cam.Current.NewFrame -= CameraNewFrame;
+
+      MediaPlayer?.Close();
+      MediaPlayer = null;
+    }
+
+    private void CameraNewFrame(object sender, NewFrameEventArgs eventArgs)
+    {
+
+      using (var newFrame = Image.Clone(eventArgs.Frame))
+      {
+
+        var reader = new BarcodeReader().Decode(newFrame);
+        Dispatcher.Invoke(() =>
+        {
+          if (reader != null )
+          {
+              QrDecoded?.Invoke(this, reader.Text);
+              Cam.Current.SignalToStop();
+          }
+          CamStream.Source = newFrame.Convert();
+        });
+      }
+    }
+
+    private void CameraIndexSelectionChanged(int e)
+    {
+      if (Cam.Devices.Count <= 0) return;
+      if (Cam.Current == null)
+      {
+        Init(e);
+      }
+      else
+      {
+        DisposeCamera();
+        Init(e);
+      }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
 }
